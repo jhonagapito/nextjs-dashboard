27aa0d265c6959935a35e050a77b682f841d637c
Jhon Agapito <jhon.agapito@gmail.com>
2023-11-03 23:21:54 +0800

    authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { sql } from '@vercel/postgres';
+import { signIn } from '@/auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
@@ -97,5 +98,16 @@ export async function deleteInvoice(id: string) {
         return { message: 'Deleted Invoice' };
     } catch (error) {
         return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+    try {
+        await signIn('credentials', Object.fromEntries(formData));
+    } catch (error) {
+        if ((error as Error).message.includes('CredentialsSignin')) {
+            return 'CredentialSignin';
+          }
+          throw error;
     }
 }
